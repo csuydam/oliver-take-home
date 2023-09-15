d04beb287bc50203c4d8299f1ca79502c1809802
Newton <altontonnalumasa@gmail.com>
2023-09-16 00:03:51 +0300

    Consume data from db.json

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/Home.scss";
+import data from '../db.json'
 
-class Home extends React.Component {
-  render() {
-    return <h1>Take Home</h1>;
-  }
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setProducts(data.products)
+  }, [])
+  
+  return (
+    <>
+      <h1>products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default Home;
