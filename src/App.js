da9cea36999ff2693e0c26fccb42a4addc249fcc
Newton <altontonnalumasa@gmail.com>
2023-09-16 16:21:11 +0300

    Display the products from json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 
-export default function OliverTakeHome() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-        </Switch>
-      </div>
-    </Router>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
 }
+export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
 import "../Styles/Home.scss";
-import data from '../db.json'
+import data from '../db.json';
+import { Link } from 'react-router-dom';
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+  font-family: sans-serif;
+`;
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -9,16 +17,17 @@ const Home = () => {
   }, [])
   
   return (
-    <>
-      <h1>products</h1>
+    <Card>
+      <h1>The products</h1>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
             <h2>{product.name}</h2>
+            <Link to={"/" + product.name}>see reviews</Link>
           </li>
         ))}
       </ul>
-    </>
+    </Card>
   )
 }
 
